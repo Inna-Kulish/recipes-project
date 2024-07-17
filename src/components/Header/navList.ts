b144261c20af67886ed9b09b7786cf84c70672fd
Inna-Kulish <norelajest@gmail.com>
2024-07-17 10:23:37 +0300

    reset useMediaQuery

---
+++ src/components/Header/navList.ts
@@ -0,0 +1,44 @@
+import Instagram from "../../assets/instagram.svg?react";
+import Facebook from "../../assets/facebook.svg?react";
+import Twitter from "../../assets/twitter.svg?react";
+
+export const NAV_LINKS = [
+  {
+    title: "Home",
+    navLink: "/",
+  },
+  {
+    title: "Recipes",
+    navLink: "/recipes",
+  },
+  {
+    title: "Blog",
+    navLink: "/blog",
+  },
+  {
+    title: "Contact",
+    navLink: "/contact",
+  },
+  {
+    title: "About us",
+    navLink: "/about",
+  },
+];
+
+export const SOCIALS_LINKS = [
+  {
+    name: "Facebook",
+    Icon: Facebook,
+    link: "https://www.facebook.com",
+  },
+  {
+    name: "Twitter",
+    Icon: Twitter,
+    link: "https://x.com",
+  },
+  {
+    name: "Instagram",
+    Icon: Instagram,
+    link: "https://www.instagram.com",
+  },
+];
